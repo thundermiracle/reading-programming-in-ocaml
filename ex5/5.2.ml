c1d57b9b4bfe36c3a2e067f2dde51eb31a796c27
thundermiracle <cissoliu@gmail.com>
2020-10-01 21:52:42 +0900

    add exercise 5.2.7,5.2.8,5.2.9

--- ex5/5.2.ml
+++ ex5/5.2.ml
@@ -52,3 +52,47 @@ let rec unzip = function
  | (x, y) :: l' -> let (l1, l2) = unzip l' in (x::l1, y::l2);;
 
 unzip (zip [2; 3; 4; 5; 6; 7; 8; 9; 10; 11] [true; true; false; true; false; true; false; false; false; true]);;
+
+
+(* 5.2.7 *)
+let rec filter p = function
+  [] -> []
+  | x::l' when p x -> x::filter p l'
+  | x::l' -> filter p l';;
+
+let is_positive x = (x>0);;
+filter is_positive [-9; 0; 2; 5; -3];;
+let rec length = function
+  [] -> 0
+  | _::l' -> 1 + length l';;
+filter (fun l -> length l = 3) [[1; 2; 3]; [4; 5]; [6; 7; 8]; [9]];;
+
+
+(* 5.2.8 *)
+let rec take n = function
+  [] -> []
+  | x::l' when n > 0 -> x::take (n-1) l'
+  | x::l' -> [];;
+
+let rec drop n = function
+  [] -> []
+  | x::l' when n > 0 -> drop (n-1) l'
+  | x::l' -> x::drop (n-1) l';;
+
+let ten_to_zero = downto1 10;;
+take 8 ten_to_zero;;
+drop 7 ten_to_zero;;
+
+
+(* 5.2.9 *)
+let max_list l =
+  let rec max_tail max = function
+    [] -> max
+    | x::tl' when x > max -> max_tail x tl'
+    | _::tl' -> max_tail max tl'
+  in
+  let first:: l' = l
+  in
+  max_tail first l';;
+
+max_list [7; 9; 0; -5];;
