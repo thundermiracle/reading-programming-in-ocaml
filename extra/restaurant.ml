e118651e6d5bd35a619dc412c96dc2216ee6b54c
thundermiracle <cissoliu@gmail.com>
2020-10-12 08:21:55 +0900

    add rec variant

--- extra/restaurant.ml
+++ extra/restaurant.ml
@@ -32,3 +32,25 @@ let isVeggieDish d =
 isVeggieDish PorkCutlet;;
 isVeggieDish (Soup {m = Aka; g = Tofu;});;
 isVeggieDish (Rice Katuo);;
+
+(* 再帰ヴァリアント *)
+type menu = Smile | Add of {d: dish; next: menu;};;
+
+let m1 = Smile;;
+let m2 = Add {d=PorkCutlet; next=m1;};;
+let m3 = Add {d=Soup {m=Aka;g=Tofu;}; next=m2};;
+let m4 = Add {d=Rice Nori;next=m3;};;
+
+let rec price_of_menu m =
+  match m with
+  Smile -> 0
+  | Add { d = d'; next = m'; } -> price_of_dish d' + price_of_menu m';;
+
+price_of_menu m4;;
+
+let rec isVeggieMenu m = 
+  match m with
+  Smile -> true
+  | Add { d = d'; next = m'} -> isVeggieDish d' && isVeggieMenu m';;
+
+isVeggieMenu m4;;
