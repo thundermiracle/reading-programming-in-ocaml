d119edd8ec628dca1a2eeb3aeaf716f5838acd2c
thundermiracle <cissoliu@gmail.com>
2020-12-01 20:13:35 +0900

    ex 6.4

---
+++ ex6/6.4.ml
@@ -0,0 +1,27 @@
+(* 6.4 *)
+type nat = Zero | OneMoreThan of nat;;
+type 'a option = None | Some of 'a;;
+
+let rec monus m n = 
+  match (m, n) with
+  (Zero, _) -> Zero
+  | (r, Zero) -> r
+  | (OneMoreThan m', OneMoreThan n') -> monus m' n';;
+
+let minus m n =
+  if m = Zero && n != Zero then None
+  else Some (monus m n);;
+
+
+(* Test *)
+let one = OneMoreThan Zero;;
+let two = OneMoreThan one;;
+
+minus Zero two;;
+(* None *)
+
+minus two one;;
+(* OneMoreThan Zero *)
+
+minus two two;;
+(* Zero *)
