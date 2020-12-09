10cb475c14bd70c06b6453ea3be1be1b32f0b0af
thundermiracle <cissoliu@gmail.com>
2020-12-09 22:30:28 +0900

    finish 6.14

--- ex6/6.14.ml
+++ ex6/6.14.ml
@@ -18,7 +18,7 @@ let is_prime_original x =
   in
   not (is_dividable_from_2 (x-1));;
 (* nthseq 3000 (prime_seq is_prime_original 3);; *)
-(* 70.62sec *)
+(* 3.88sec *)
 
 
 (***************** Divide from 2 to x-1 Version *****************)
@@ -28,7 +28,7 @@ let is_prime_small_to_big x =
   in
   not (is_dividable_from_2 2);;
 (* nthseq 3000 (prime_seq is_prime_small_to_big 3);; *)
-(* 10.72sec *)
+(* 0.57sec *)
 
 
 (***************** Divide from 2 to sqrt x Version *****************)
@@ -39,6 +39,30 @@ let is_prime_small_to_sqrt x =
   in
   not (is_dividable_from_2 2);;
 (* nthseq 3000 (prime_seq is_prime_small_to_sqrt 3);; *)
-(* 0.12sec *)
+(* 0.027sec *)
+
+
+
+let rec prime_seq_primes is_prime_by_primes primes x =
+  if is_prime_by_primes primes (x+1) then Cons(x+1, prime_seq_primes is_prime_by_primes ((x+1)::primes))
+  else prime_seq_primes is_prime_by_primes primes (x+1);;
+(***************** Divide from 2 to x which are primes Version *****************)
+let rec is_prime_small_to_primes primes x =
+  match primes with
+  [] -> true
+  | p :: rest -> p < x && x mod p <> 0 && is_prime_small_to_primes rest x;;
+(* nthseq 3000 (prime_seq_primes is_prime_small_to_primes [] 1);; *)
+(* 0.90sec *)
+
+
+(***************** Divide from 2 to sqrt x which are primes Version *****************)
+let rec is_prime_small_to_sqrt_primes primes x =
+  let xTo = int_of_float (floor (sqrt (float_of_int x))) in
+  match primes with
+  [] -> true
+  | p :: rest when p >= xTo -> is_prime_small_to_primes rest x
+  | p :: rest -> x mod p <> 0 && is_prime_small_to_primes rest x;;
+(* nthseq 3000 (prime_seq_primes is_prime_small_to_sqrt_primes [] 1);; *)
+(* 0.99sec *)
 
 Printf.printf "Execution time: %fs\n" (Sys.time() -. t);;
