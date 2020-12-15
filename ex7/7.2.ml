f7f400d96318d8544bb31af000495041a5998959
thundermiracle <cissoliu@gmail.com>
2020-12-15 17:58:20 +0900

    ex 7.2

---
+++ ex7/7.2.ml
@@ -0,0 +1,14 @@
+(* 7.2 *)
+exception Multiple_zero;;
+
+let prod_list l =
+  let rec prod_list' = function
+    [] -> 1
+    | a::rest when a = 0 -> raise Multiple_zero
+    | a::rest -> a * (prod_list' rest)
+  in
+  try prod_list' l with Multiple_zero -> 0;;
+
+prod_list [2;4;5;];;
+
+prod_list [2;4;0;5;6;7;8;];
